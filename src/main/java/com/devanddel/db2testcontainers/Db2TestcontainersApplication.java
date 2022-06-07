e132b0126c26eec5b25b8b90544ec5c56e643bf5
carlos mugica <carlos@devanddel.com>
2022-06-07 09:59:36 +0200

    Initial commit

---
+++ src/main/java/com/devanddel/db2testcontainers/Db2TestcontainersApplication.java
@@ -0,0 +1,13 @@
+package com.devanddel.db2testcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2TestcontainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db2TestcontainersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devanddel/db2testcontainers/Db2TestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devanddel.db2testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db2TestcontainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
